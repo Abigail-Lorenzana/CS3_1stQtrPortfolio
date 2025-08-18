184dd92c061efe6dbe0029c7bf3f403cae1ac41f
Abigail-Lorenzana <b2029amlorenzana@pshs.edu.ph>
2025-08-19 05:09:12 +0800

    commit

--- public/index.js
+++ public/index.js
@@ -1,14 +1,9 @@
-<html>
-<body>
 
-    <script>
+<script>
     let age = 16;
     let maxAge = 100;
     let numPerDay = 2;
     let totalRequired = (numPerDay*365)*(maxAge-age);
 
-    document.write (`You will need ${totalRequired} to last you until the ripe age of ${maxAge}`);
-    </script>
-
-</body>
-</html>
+    document.write(`You will need ${totalRequired} to last you until the ripe age of ${maxAge}`);
+</script>
